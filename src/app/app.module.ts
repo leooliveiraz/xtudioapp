5ab902f848b4c6c893cbf1b198cb5d9d956fc325
Leonardo Rocha <leopdor@gmail.com>
2019-04-27 13:10:17 -0300

    Criado Header e Sidenav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { 
+    MzNavbarModule,
+    MzIconModule, 
+    MzIconMdiModule,
+    MzSidenavModule, 
+    MzButtonModule } from 'ngx-materialize';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './template/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MzNavbarModule,
+    MzIconModule, 
+    MzIconMdiModule,
+    MzSidenavModule,
+    MzButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
